60ccf0ca90f276129c02443e0dcba1b6a741b2c9
Brian McKeon <bmckeon@layer3com.com>
2024-03-06 14:39:10 -0500

    Prepare 0.8.2.1 release. Removed other subprojects. Reformatted. Updated changelog and readme. Use new .github workflows.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Revision history for siphon
+
+## 0.8.2.1 -- 2024-03-06
+
+* Update package metadata.
+* Removed `semigroups` dependency.
+
+## 0.8.2.0 -- 2022-10-11
+
+* Add
+
+## 0.8.1.2 -- 2021-10-25
+
+* Correct handling of CRLF.
---
+++ src/Siphon/Text.hs
@@ -0,0 +1,39 @@
+module Siphon.Text where
+
+import Data.Coerce (coerce)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Siphon (Escaped)
+import Siphon.Types
+
+import qualified Data.Text as Text
+import qualified Data.Vector as Vector
+
+siphon :: Siphon Text
+siphon =
+  Siphon
+    escape
+    encodeRow
+    (error "siphon: uhoent")
+    (error "siphon: uheokj")
+
+encodeRow :: Vector (Escaped Text) -> Text
+encodeRow =
+  id
+    . Text.intercalate (Text.singleton ',')
+    . Vector.toList
+    . coerce
+
+escape :: Text -> Escaped Text
+escape t = case Text.find (\c -> c == '\n' || c == ',' || c == '"') t of
+  Nothing -> Escaped t
+  Just _ -> escapeAlways t
+
+escapeAlways :: Text -> Escaped Text
+escapeAlways t =
+  Escaped $
+    Text.concat
+      [ Text.singleton '"'
+      , Text.replace (Text.pack "\"") (Text.pack "\"\"") t
+      , Text.singleton '"'
+      ]
